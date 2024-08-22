18d022feeab0dd279b5a1e2c15cb237a0c0e496c
Chodipilli Ajay <115925992+chodipilliajay@users.noreply.github.com>
2024-08-22 21:30:44 +0530

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+<div>
+    <h1>HELLO AJAY</h1>
+    <p>Have a nice day....</p>
+</div>,
+document.getElementById("root")
+);
